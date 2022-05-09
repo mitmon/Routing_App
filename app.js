7abbaf11a9a70cbee0785f974fb756283bba398f
mitmon <mitchkrafczek@outlook.com>
2022-05-09 15:08:08 -0400

    fixed edits

--- app.js
+++ app.js
@@ -1,7 +1,91 @@
 // default map layer
-// 'map' refers to a <div> element with the ID map
 let map = L.map('map', {
+  layers: MQ.mapLayer(),
   center: [37.7749, -122.4194],
-  layers: MQ.tileLayer('map'),
   zoom: 12
 });
+  
+
+function runDirection(start, end) {
+    
+    // recreating new map layer after removal
+    map = L.map('map', {
+        layers: MQ.mapLayer(),
+        center: [37.7749, -122.4194],
+        zoom: 12
+    });
+    
+    var dir = MQ.routing.directions();
+
+    dir.route({
+        locations: [
+            start,
+            end
+        ]
+    });
+
+
+    CustomRouteLayer = MQ.Routing.RouteLayer.extend({
+        createStartMarker: (location) => {
+            var custom_icon;
+            var marker;
+
+            custom_icon = L.icon({
+                iconUrl: 'img/home_pin.svg',
+                iconSize: [32, 32],
+                iconAnchor: [10, 29],
+                popupAnchor: [0, -29]
+            });
+
+            marker = L.marker(location.latLng, {icon: custom_icon}).addTo(map);
+
+            return marker;
+        },
+
+        createEndMarker: (location) => {
+            var custom_icon;
+            var marker;
+
+            custom_icon = L.icon({
+                iconUrl: 'img/personal_pin_circle.svg',
+                iconSize: [32, 32],
+                iconAnchor: [10, 29],
+                popupAnchor: [0, -29]
+            });
+
+            marker = L.marker(location.latLng, {icon: custom_icon}).addTo(map);
+
+            return marker;
+        }
+    });
+    
+    map.addLayer(new CustomRouteLayer({
+        directions: dir,
+        fitBounds: true
+    })); 
+}
+
+
+// function that runs when form submitted
+function submitForm(event) {
+  event.preventDefault();
+
+  // delete current map layer
+  map.remove();
+
+  // getting form data
+  start = document.getElementById("start").value;
+  end = document.getElementById("end").value;
+
+  // run directions function
+  runDirection(start, end);
+
+  // reset form
+  document.getElementById("form").reset();
+}
+
+// asign the form to form variable
+const form = document.getElementById('form');
+
+// call the submitForm() function when submitting the form
+form.addEventListener('submit', submitForm);
